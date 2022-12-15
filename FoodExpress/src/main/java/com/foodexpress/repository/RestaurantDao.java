d11ab1eac2f82395a001e608bdc5adfe3209457b
Shivendra Shrivastwa <shivendrasince2001@gmail.com>
2022-12-15 22:26:07 +0530

    fw20_0384-day-4 committed

--- FoodExpress/src/main/java/com/foodexpress/repository/RestaurantDao.java
+++ FoodExpress/src/main/java/com/foodexpress/repository/RestaurantDao.java
@@ -8,11 +8,10 @@ import com.foodexpress.model.Restaurants;
 
 @Repository
 public interface RestaurantDao extends JpaRepository<Restaurants, Integer> {
-
-	//public Restaurants findByrestaurantName(String restaurantName);
 	
 	@Query("select r from Restaurants r where r.restaurantName=?1")
 	public Restaurants getResByName(String restaurantName);
 	
 	public Restaurants findByContactNumber(String contactNumber);
 }
+ 
